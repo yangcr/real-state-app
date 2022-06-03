e37d73d156038da664a6715090762aa7e8661aa9
Cristopher Yang <dg.cris.yang@gmail.com>
2022-06-03 20:26:31 -0300

    created searchbox

---
+++ frontend/Components/ListingContainer/ListingContainer.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import ListingCard from "../ListingCard.jsx"
+import ListingContainerStyles from "./ListingContainerStyles.module.css";
+
+function ListingContainer() {
+  return (
+    <>
+    <div className={ListingContainerStyles.listingContainer}>
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+      <ListingCard />
+    </div>
+    </>
+  );
+}
+
+export default ListingContainer;
---
+++ frontend/Components/SearchBox/SearchBox.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Input, Select, Form } from "antd";
+import SearchBoxStyles from "./SearchBox.module.css";
+const { Option } = Select;
+
+function SearchBox() {
+  return (
+    <>
+      <Form layout="vertical" className={SearchBoxStyles.formContainer}>
+          <div className={SearchBoxStyles.formGroup}>
+          <Form.Item label="Operation type">
+            <Select
+              defaultValue="all"
+              style={{
+                width: 120,
+              }}
+            >
+              <Option value="all">All</Option>
+              <Option value="buy">Buy</Option>
+              <Option value="rent">Rent</Option>
+            </Select>
+          </Form.Item>
+
+          <Form.Item label="Property type">
+            <Select
+              defaultValue="all"
+              style={{
+                width: 120,
+              }}
+            >
+              <Option value="all">All</Option>
+              <Option value="house">House</Option>
+              <Option value="apartment">Apartment</Option>
+            </Select>
+          </Form.Item>
+          </div>
+          <div className={SearchBoxStyles.formGroup}>
+          <Form.Item label="Min. price">
+            <Input
+              placeholder="e.g: 50.000"
+              allowClear
+              type="number"
+              className={SearchBoxStyles.searchInput}
+            />
+          </Form.Item>
+
+          <Form.Item label="Max. price">
+            <Input
+              placeholder="e.g: 500.000"
+              allowClear
+              type="number"
+              className={SearchBoxStyles.searchInput}
+            />
+          </Form.Item>
+          </div>
+
+          <Form.Item label="Min. floor area (in m2)">
+            <Input
+              placeholder="e.g: 75"
+              allowClear
+              type="number"
+              className={SearchBoxStyles.searchInput}
+            />
+          </Form.Item>
+      </Form>
+    </>
+  );
+}
+
+export default SearchBox;
