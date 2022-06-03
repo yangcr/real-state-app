09e75ebaf0d6bc8d2785b4c97733002684b1543a
Cristopher Yang <dg.cris.yang@gmail.com>
2022-06-03 19:00:06 -0300

    fixed indenting

--- frontend/Components/ListingCard.jsx
+++ frontend/Components/ListingCard.jsx
@@ -12,28 +12,30 @@ const CardListing = () => (
       }}
       cover={
         <img
-          alt="example"
+          alt="image of the living room of a house"
           src="https://images.unsplash.com/photo-1613545325278-f24b0cae1224?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170"
         />
       }
     >
       <div className={ListingCardStyles.cardContent}>
-        <div >
+        <div>
           <p className={ListingCardStyles.headerSubtitle}>
             Ciudad Autónoma de Buenos Aires
           </p>
           <div className={ListingCardStyles.cardHeader}>
-            <h2 className={ListingCardStyles.headerTitle}>Departamento 3 ambientes</h2>
+            <h2 className={ListingCardStyles.headerTitle}>
+              Departamento 3 ambientes
+            </h2>
             <p className={ListingCardStyles.cardPrice}>$135.000</p>
           </div>
         </div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quo, ex
-            aliquid eum blanditiis et fuga officia reiciendis totam dolorem
-            cupiditate maiores maxime provident voluptates! Ab amet possimus
-            deserunt facere?.
-          </p>
-<CardCharacteristics/>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quo, ex
+          aliquid eum blanditiis et fuga officia reiciendis totam dolorem
+          cupiditate maiores maxime provident voluptates! Ab amet possimus
+          deserunt facere?.
+        </p>
+        <CardCharacteristics />
       </div>
     </Card>
   </>
