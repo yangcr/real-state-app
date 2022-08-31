0bc318fa30f41431a12787c875eadf168c6015d1
Cristopher Yang <dg.cris.yang@gmail.com>
2022-08-31 20:34:12 -0300

    change dynamically filter background depending on operation type

--- frontend/Components/SearchBox/SearchBox.jsx
+++ frontend/Components/SearchBox/SearchBox.jsx
@@ -1,27 +1,37 @@
-import React from "react";
-import { Input, Select, Form, Button } from "antd";
+import React, {useState} from "react";
+import { Input, Select, Form, Radio } from "antd";
 import styles from "./SearchBox.module.css";
-import { useState } from "react";
 const { Option } = Select;
 
 function SearchBox() {
 
-  function selectTransactionType(value) {
-    setTransactionType(value);
-  }
+  const options = [
+    {
+      label: 'ALL',
+      value: 'all',
+    },
+    {
+      label: 'BUY',
+      value: 'buy',
+    },
+    {
+      label: 'RENT',
+      value: 'rent',
+    },
+  
+  ];
+
   const [operationType, setOperationType] = useState("all");
 
   return (
     <div className={styles.searchBoxComponent}>
-      <Button className={[styles.buttonForm, styles.buttonBuy]}>
-        Buy
-      </Button>
-      <Button className={[styles.buttonForm, styles.buttonRent]}>
-        Rent
-      </Button>
-      <Button className={[styles.buttonForm, styles.buttonAll]}>
-        All
-      </Button>
+      <Radio.Group
+        options={options}
+        optionType="button"
+        buttonStyle="solid"
+        onChange={(e) => setOperationType(e.target.value)}
+        className={styles.operationRadioGroup}
+      />
       <Form layout="vertical" className={[styles.formContainer, operationType == "buy" ? styles.containerBuy : operationType == "rent" ? styles.containerRent : styles.containerAll]}>
         <div className={styles.formGroup}>
           <Form.Item label="Property type">
